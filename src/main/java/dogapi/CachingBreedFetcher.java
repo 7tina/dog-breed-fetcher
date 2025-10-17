3e69a6098700fac941bccfcfaba253466ba7bb45
Tina Ly <t.ly@mail.utoronto.ca>
2025-10-17 02:40:03 -0400

    completed tasks

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -2,27 +2,34 @@ package dogapi;
 
 import java.util.*;
 
-/**
- * This BreedFetcher caches fetch request results to improve performance and
- * lessen the load on the underlying data source. An implementation of BreedFetcher
- * must be provided. The number of calls to the underlying fetcher are recorded.
- *
- * If a call to getSubBreeds produces a BreedNotFoundException, then it is NOT cached
- * in this implementation. The provided tests check for this behaviour.
- *
- * The cache maps the name of a breed to its list of sub breed names.
- */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
+    private final BreedFetcher fetcher;
+    private final Map<String, List<String>> cache = new HashMap<>();
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
 
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        this.fetcher = fetcher;
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedFetcher.BreedNotFoundException {
+        String key = breed.toLowerCase();
+
+        // Check cache first
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+
+        // Always count that we're making a call to the underlying fetcher
+        callsMade++;
+
+        try {
+            List<String> subBreeds = fetcher.getSubBreeds(key);
+            cache.put(key, subBreeds);
+            return subBreeds;
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            throw e;
+        }
     }
 
     public int getCallsMade() {
